1899554ef6aa63b1f88a505ffb929d2e8b1b14e8
arthurgehrke <arthurgehrke07@gmail.com>
2020-04-26 18:02:52 -0300

    chore: initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+const axios = require('axios')
+const cheerio = require('cheerio')
+
+const app = express()
+
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app,
+  watch: true
+})
+
+app.use(express.urlencoded({ extended: false }))
+app.set('view engine', 'njk')
+
+const artigos = []
+const legislacoes = []
+const numeros = []
+const orgaos = []
+
+app.get('/', (req, res) => {
+  res.render('articles', {})
+})
+
+app.post('/find', (req, res) => {
+  artigos.push(req.body.artigo)
+  legislacoes.push(req.body.legislacao)
+  return res.redirect('/')
+})
+
+console.log(artigos.length)
+
+app.listen(4000)
